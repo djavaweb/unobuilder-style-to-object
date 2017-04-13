7a210227e3afc34ce3623a28c9b580a550321dad
oknoorap <oknoorap@gmail.com>
2017-04-13 13:04:25 +0700

    fix: test file
    six tests are enough

--- test/test.js
+++ test/test.js
@@ -5,22 +5,31 @@ import nativeProps from './native-props'
 const object = cssToProps(nativeProps)
 
 test('backgroundColor is an object', t => {
-  t.true(typeof object === 'object')
+  t.is(typeof object, 'object')
 })
 
-test('backgroundColor.rgba is an object', t => {
-  t.true(typeof object.value.rgba === 'object')
+test('backgroundColor rgba is an object', t => {
+  t.is(typeof object.backgroundColor.value.rgba, 'object')
 })
 
-test('backgroundColor.rgba.r is a number', t => {
-  t.true(typeof object.value.rgba.r === 'number')
-  t.true(typeof object.value.rgba.r > -1)
+test('backgroundColor rgba.r is a number', t => {
+  const typeOfRed = typeof object.backgroundColor.value.rgba.r
+  t.is(typeOfRed, 'number')
+  t.true(object.backgroundColor.value.rgba.r > -1)
 })
 
 test('display should be `block`', t => {
-  t.true(object.display.value === 'block')
+  t.is(object.display.value, 'block')
 })
 
 test('width should have a unit', t => {
-  t.true(object.width.unit)
+  const unitType = typeof object.width.unit
+  t.is(unitType, 'string')
+  t.true(object.width.unit.length > 0)
+})
+
+test('fontSize should have a unit', t => {
+  const unitType = typeof object.fontSize.unit
+  t.is(unitType, 'string')
+  t.true(object.fontSize.unit.length > 0)
 })
