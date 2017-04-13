bb94fb9f707e28d94c32ddd49e992d3213b29fd4
oknoorap <oknoorap@gmail.com>
2017-04-13 12:34:44 +0700

    fix: main file content

--- index.js
+++ index.js
@@ -1,3 +1,57 @@
-module.exports = nativeStyle => {
+const tinycolor = require('tinycolor2')
+const availableUnits = [
+  'px',
+  'pt',
+  'em',
+  'rem',
+  'vh',
+  'vw',
+  '%'
+]
+const unitRegx = new RegExp(`(\\d+)(${availableUnits.join('|')})`)
+const rgbRegx = /(rgba?)\((.*)\)/
 
+module.exports = nativeStyle => {
+  const object = {}
+  for (const key in nativeStyle) {
+    if (typeof key !== 'number') {
+      let value = nativeStyle[key]
+      let unit
+
+      const matchUnit = value.match(unitRegx)
+      if (matchUnit) {
+        const int = parseInt(matchUnit[1])
+        if (!isNaN(int)) {
+          value = int
+          unit = matchUnit[2]
+        }
+      }
+
+      const matchColor = value.match(rgbRegx)
+      if (matchColor) {
+        const source = matchColor[1]
+        const colors = matchColor[2].split(',')
+        const a = (source === 'rgba') ? colors[colors.length - 1] : 0
+        const color = tinycolor(value)
+        const hsl = color.toHsl()
+        value = {
+          hsl,
+          source,
+          a,
+          hex: color.toHexString().toUpperCase(),
+          rgba: color.toRgb(),
+          hsv: color.toHsv(),
+          oldHue: hsl.h
+        }
+      }
+
+      object[key] = {
+        value
+      }
+
+      if (unit) {
+        object[key].unit = unit
+      }
+    }
+  }
 }
